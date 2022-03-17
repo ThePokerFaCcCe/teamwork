61e9d0775a08c9798bf999a163f9cd9d6aa0e231
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-17 18:23:11 +0330

    :truck: Add `get_team_with_members` query func

--- team/querysets.py
+++ team/querysets.py
@@ -9,3 +9,10 @@ def get_user_teams(user_id) -> QuerySet:
         .filter(user_id=user_id).
         values_list('team_id', flat=True)
     ).select_related("creator")
+
+
+def get_team_with_members(team_id) -> QuerySet:
+    """Return team and it's members with tasks prefetched"""
+    return Team.objects.filter(id=team_id)\
+        .prefetch_related("members__tasks", "members__user")\
+        .first()
