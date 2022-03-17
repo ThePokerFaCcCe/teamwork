6b16aac342aa8dea6fa3be507d6ef695ff91b487
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-17 15:54:01 +0330

    :truck: Add `get_user_teams` query func

---
+++ team/querysets.py
@@ -0,0 +1,11 @@
+from django.db.models import QuerySet
+from .models import Team, Member
+
+
+def get_user_teams(user_id) -> QuerySet:
+    """Return teams that user joined/created"""
+    return Team.objects.filter(
+        id__in=Member.objects.only('team_id')
+        .filter(user_id=user_id).
+        values_list('team_id', flat=True)
+    )
