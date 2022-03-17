3b877dcfbb38e87a5f743abf4a150d0469278334
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-17 15:26:22 +0330

    :pencil2: Change `UsernameValidator`'s error message

--- user/validators.py
+++ user/validators.py
@@ -8,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 class UsernameValidator(validators.RegexValidator):
     regex = r'^[A-Za-z]{1,}[_]?[A-Za-z0-9]{1,}$'
     message = _(
-        'Invalid username! it must start with English letters '
-        'and only can contain English letters, digits and one underscore( _ )'
+        'Must start with English letters. '
+        'can contain digits and one "_"'
     )
     flags = re.ASCII
