4fbfbf9a5e3511081aac50eb6f884744aac971a1
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-17 16:55:26 +0330

    :sparkles: Add team views

---
+++ team/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Team
+
+
+class TeamCreateForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = [
+            'name',
+            'description',
+            'creator',
+        ]
