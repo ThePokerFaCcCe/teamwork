8949ff603581d48bdf5946235681f9d76dfcb17e
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-18 19:07:43 +0330

    :sparkles: Add `TaskCreateForm`

---
+++ task/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.utils import timezone
+from .models import Task
+
+
+class TaskCreateForm(forms.ModelForm):
+    deadline = forms.IntegerField(max_value=366, min_value=1)
+
+    class Meta:
+        model = Task
+        fields = [
+            "title",
+            "description",
+            "member",
+            "team",
+            "deadline",
+        ]
+
+    def clean_deadline(self):
+        deadline_days = self.cleaned_data['deadline']
+        return timezone.timedelta(days=deadline_days) + timezone.now()
+
+    def full_clean(self):
+        super().full_clean()
+        if self._errors:
+            return
+
+        if self.cleaned_data['member'].team_id != self.cleaned_data['team'].pk:
+            self.add_error("member", 'Member not found')
